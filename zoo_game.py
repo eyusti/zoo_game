eedc04b1496a9cbd05934ffd2e23fdebead449be
Ellen <eyusti@gmail.com>
2020-04-08 15:32:18 -0400

    Updated so there is a main function and so that turtle doesn't show at origin on initialization

--- zoo_game.py
+++ zoo_game.py
@@ -1,40 +1,41 @@
 import turtle
 
+# Available Sprites
 dinosaurs = {
     "triceratops": "trippy.gif",
     "stegosaurus": "steggy.gif",
-    # "trex": "rex.gif",
 }
 
-#Background
-turtle.title("Dinosaur Zoo")
-screen = turtle.getscreen()
+# Screen Settings
+screen = turtle.Screen()
+screen.title("Dinosaur Zoo")
 screen.setup(1000, 625)
 screen.bgpic("grass.gif")
-screen.delay(750)
 screen.addshape(dinosaurs['triceratops'])
 screen.addshape(dinosaurs['stegosaurus'])
 
-#Make A dinosaur
+# Dinosaur Functions
 def create_dino(dino_type):
     dino = turtle.Turtle()
     dino.shape(dinosaurs[dino_type])
+    return dino
 
-dino_1 = create_dino('triceratops')
-dino_2 = create_dino('stegosaurus')
-
-#Dinosaur Movement
-"""
 def move_dino(dino):
-    # dino.penup()
+    dino.penup()
     dino.speed("slowest")
     while True:
-        dino.backward(100)
-        dino.left(90)
-        dino.backward(100)
-        dino.left(180)
-        dino.backward(200)
+        dino.fd(100)
+        dino.lt(90)
+        dino.fd(100)
+        dino.lt(180)
+        dino.fd(200)
 
-move_dino(dino_1)
-"""
-input()
+# Testing execution
+def main():
+    dino_1 = create_dino('triceratops')
+    screen.delay(750)
+    #dino_2 = create_dino('stegosaurus')
+    move_dino(dino_1)
+
+if __name__ == "__main__":
+    main()
