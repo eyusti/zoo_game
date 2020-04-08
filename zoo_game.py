e015c8a78dc405963b94f50f5c8a4e42c53d5bdf
Ellen <eyusti@gmail.com>
2020-04-08 17:00:39 -0400

    Added collision detection for screen edges

--- zoo_game.py
+++ zoo_game.py
@@ -8,9 +8,11 @@ dinosaurs = {
 }
 
 # Screen Settings
+width = 1000
+height = 624
 screen = turtle.Screen()
 screen.title("Dinosaur Zoo")
-screen.setup(1000, 624)
+screen.setup(width, height)
 screen.bgpic("grass.gif")
 screen.addshape(dinosaurs['triceratops'])
 screen.addshape(dinosaurs['stegosaurus'])
@@ -19,10 +21,8 @@ screen.addshape(dinosaurs['stegosaurus'])
 def create_dino(dino_type):
     dino = turtle.Turtle(shape=dinosaurs[dino_type], visible=False)
     dino.penup()
-    x = random.randint(-500,500)
-    y = random.randint(-312,312)
-    print(x)
-    print(y)
+    x = random.randint(-width/2,width/2)
+    y = random.randint(-height/2,height/2)
     dino.setposition(x,y)
     dino.showturtle()
     return dino
@@ -31,27 +31,37 @@ def move_dino(dino):
     dino.penup()
     dino.speed("slowest")
     movement = random.randint(0,4)
-    distance = random.randint(200,300)
-    print(movement)
-    print(distance)
+    distance = random.randint(200,400)
     if movement == 0:
-        dino.forward(distance)
+        if dino.xcor() + distance < width/2:
+            dino.forward(distance)
+        else:
+            dino.forward(distance + (width/2 -(dino.xcor() + distance)))
     elif movement == 1:
-        dino.backward(distance)
+        if dino.xcor() - distance > -width/2:
+            dino.backward(distance)
+        else:
+            dino.backward(distance + (width/2 + (dino.xcor() - distance)))
     elif movement == 2:
-        dino.setx(distance)
+        if dino.ycor() - distance > -height/2:
+            dino.sety(-distance)
+        else:
+            dino.sety(-distance - ((dino.ycor() - distance)+ height/2))
     elif movement == 3:
-        dino.sety(distance)
+        if dino.ycor() + distance < height/2:
+            dino.sety(distance)
+        else:
+            dino.sety(distance - ((dino.ycor() + distance) - height/2))
     else:
-        x = random.randint(-500,500)
-        y = random.randint(-312,312)
+        x = random.randint(-width/2,width/2)
+        y = random.randint(-height/2,height/2)
         dino.goto(x,y)
 
 # Testing execution
 def main():
     dino_1 = create_dino('triceratops')
     dino_2 = create_dino('stegosaurus')
-    screen.delay(750)
+    #screen.delay(750)
     while True:
         move_dino(dino_1)
         move_dino(dino_2)
